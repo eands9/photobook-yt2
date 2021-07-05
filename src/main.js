9c39c30e77ba2ff1f62e8d9bcdff9b6dda2e8126
“Eric <eands9@yahoo.com>
2021-07-04 20:37:47 -0500

    26:58 before configuring for auth store

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/tailwind.css'
+import vuetify from './plugins/vuetify'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+import aws_exports from './aws-exports'
+
+Amplify.configure(aws_exports);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
